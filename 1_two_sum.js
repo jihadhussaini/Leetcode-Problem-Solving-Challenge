71dba5227b73de7d7a7c4c1e7ac865e299f12f3b
Jihad Hussaini <jihad.akbar.hussaini@gmail.com>
2023-01-07 18:10:37 +0700

    two sum solution

---
+++ 1_two_sum.js
@@ -0,0 +1,40 @@
+// problem
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example :
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+let numss = [2, 11, 15, 9, 5, 7]
+let targett = 9
+
+// common solution
+function first_solution(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]
+            }
+        }
+    }
+}
+// improved solution => better big O Notation in terms of time complexity
+function twoSum(nums, target) {
+    let temp_obj = {}
+    for (let i = 0; i < nums.length; i++) {
+        let rest = target - nums[i];        
+        if (temp_obj[rest] !== undefined) {
+            console.log("masuk if")
+            return [temp_obj[rest], i]
+        }
+        temp_obj[nums[i]] = i;
+    }
+}
+
+console.log(twoSum(numss, targett))
+console.log(first_solution(numss, targett))
