69aedbbfb2145b4ac447e8f2f957d8e5dea11ab2
Jihad Hussaini <jihad.akbar.hussaini@gmail.com>
2023-01-13 10:09:38 +0700

    find longest common prefix and valid parentheses

---
+++ 5_valid_parentheses.js
@@ -0,0 +1,62 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "([}}])"
+Output: false
+*/
+
+let s = "([}}])"
+var isValid = function(s) {
+    let temp = []
+    if (s.length % 2 !== 0) return false;
+    
+    for (let i=0; i<s.length; i++) {
+        
+        if (s[i] == "(" || s[i] == "[" || s[i] == "{") {
+            temp.push(s[i])
+        }
+        //
+        if (s[i] == ")" && temp.includes("(") == false) {
+            return false
+        }
+        if (s[i] == "]" && temp.includes("[") == false) {
+            return false
+        }
+        if (s[i] == "}" && temp.includes("{") == false) {
+            return false
+        }
+        //
+        if (s[i] == ")" && temp[temp.length-1] == "(") {
+            temp.pop()
+        }
+        if (s[i] == "]" && temp[temp.length-1] == "[") {
+            temp.pop()
+        }
+        if (s[i] == "}" && temp[temp.length-1] == "{") {
+            temp.pop()
+        }
+        
+        
+        
+    }
+    if (temp.length == 0) return true;
+    else return false;
+};
+
+console.log(isValid(s))
